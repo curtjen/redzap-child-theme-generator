a380f92cb5808f1a7e8d48a59c43ae87e67fe724
Garth <jmort47@hotmail.com>
2014-09-17 09:23:10 -0600

    Initial commit.

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== RZ Child Theme === 
+Contributors: voldemortensen, (add curtis)
+Tags: theme, creation, new, child-theme
+Requires at least: 3.0
+Tested up to: 4.0
+Stable Tag: trunk
+License: GPLv2
+License URI: http://www.opensource.org/licenses/GPL-2.0
+Version: 1.0
+
+A plugin that allows you to create a child theme based on the template of your
+choice.
+
+== Description ==
+
+A plugin that allows you to create a child theme based on the template of your
+choice.
+
+Install. Click on Tools > Create a Child Theme. Fill out the form. Submit.
+
+After the child theme is created, you'll be taken directly to the Theme Editor screen to make modifications to your child themes stylesheet.
+
+
+== Installation ==
+
+1. Upload the files to the `/wp-content/plugins/redzap-child-theme-generator/` directory or install through WordPress directly.
+1. Activate the "Child Theme" plugin through the 'Plugins' menu in WordPress
+1. Try the "Create a Child Theme" option in the Tools menu.
+
+
+== Changelog ==
+
+= 1.0 = 
+* First version.
---
+++ redzap-child-theme-generator.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: RedZap Child Theme Generator
+Plugin URI: 
+Description: A plugin to create child themes based off an installed theme.
+Version: 1.0
+Author: voldemortensen, (add curtis)
+Author URI: 
+Text Domain: rzct
+License: GPLv2 only
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2,
+    as published by the Free Software Foundation.
+
+    You may NOT assume that you can use any other version of the GPL.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    The license for this software can likely be found here:
+    http://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+// Load the textdomain
+add_action('init', 'rzct_load_textdomain');
+function rzct_load_textdomain() {
+	load_plugin_textdomain('rzct', false, dirname(plugin_basename(__FILE__)));
+}
+
+
+add_action('admin_menu', 'rzct_admin_add_page');
+function rzct_admin_add_page() {
+	add_submenu_page(
+		'tools.php',
+		esc_html__('Create a Child Theme','rzct'),
+		esc_html__('Create a Child Theme','rzct'),
+		'switch_themes',
+		'rzct',
+		'rzct_options_page'
+	);
+}
+
+function rzct_options_page() {
+	$results = rzct_create_plugin();
+
+	if ( $results === true ) return;
+	?>
+	<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2><?php esc_html_e('Create a Child Theme','rzct'); ?></h2>
+		<?php settings_errors(); ?>
+		<form method="post" action="">
+		<?php wp_nonce_field('rzct_nonce'); ?>
+		<table class="form-table">
+		<?php
+		$opts = array(
+			'name' => esc_html__('Theme Name', 'rzct'),
+			'template' => esc_html__('Template', 'rzct'),
+			'uri' => esc_html__('Theme URI (optional)', 'rzct'),
+			'description' => esc_html__('Description (optional)', 'rzct'),
+			'version' => esc_html__('Version (optional)', 'rzct'),
+			'author' => esc_html__('Author (optional)', 'rzct'),
+			'author_uri' => esc_html__('Author URI (optional)', 'rzct'),
+		);
+
+		foreach ($opts as $slug=>$title) {
+			$value = '';
+			if (!empty($results['rzct_'.$slug])) $value = esc_attr($results['rzct_'.$slug]);
+			echo "<tr valign='top'><th scope='row'>{$title}</th><td><input class='regular-text' type='text' name='rzct_{$slug}' value='{$value}'></td></tr>\n";
+		}
+		?>
+		</table>
+		<?php submit_button( esc_html__('Create a child theme and activate it!', 'rzct') ); ?>
+		</form>
+	</div>
+<?php
+}
+
+function rzct_create_plugin() {
+	if ( 'POST' != $_SERVER['REQUEST_METHOD'] )
+		return false;
+
+	check_admin_referer('rzct_nonce');
+
+	// remove the magic quotes
+	$_POST = stripslashes_deep( $_POST );
+
+	if (empty($_POST['rzct_name'])) {
+		add_settings_error( 'rzct', 'required_name',esc_html__('Theme Name is required', 'rzct'), 'error' );
+		return $_POST;
+	}
+
+	if (empty($_POST['rzct_template'])) {
+		add_settings_error( 'rzct', 'required_template', esc_html__('Template is required.', 'rzct'), 'error' );
+		return $_POST;
+	}
+
+	if ( empty($_POST['rzct_slug'] ) ) {
+		$_POST['rzct_slug'] = sanitize_title($_POST['rzct_name']);
+	} else {
+		$_POST['rzct_slug'] = sanitize_title($_POST['rzct_slug']);
+	}
+
+	if ( file_exists(trailingslashit(WP_PLUGIN_DIR).$_POST['rzct_slug'] ) ) {
+		add_settings_error( 'rzct', 'existing_theme', esc_html__('That theme appears to already exist. Use a different slug or name.', 'rzct'), 'error' );
+		return $_POST;
+	}
+
+	$form_fields = array ('rzct_name', 'rzct_uri', 'rzct_description', 'rzct_version',
+				'rzct_author', 'rzct_author_uri');
+	$method = ''; // TODO TESTING
+
+	// okay, let's see about getting credentials
+	$url = wp_nonce_url('tools.php?page=rzct','rzct_nonce');
+	if (false === ($creds = request_filesystem_credentials($url, $method, false, false, $form_fields) ) ) {
+		return true;
+	}
+
+	// now we have some credentials, try to get the wp_filesystem running
+	if ( ! WP_Filesystem($creds) ) {
+		// our credentials were no good, ask the user for them again
+		request_filesystem_credentials($url, $method, true, false, $form_fields);
+		return true;
+	}
+
+
+	global $wp_filesystem;
+
+	// create the plugin directory
+	$themedir = $wp_filesystem->wp_themes_dir() . $_POST['rzct_slug'];
+
+	if ( ! $wp_filesystem->mkdir($themedir) ) {
+		add_settings_error( 'rzct', 'create_directory', esc_html__('Unable to create the theme directory.', 'rzct'), 'error' );
+		return $_POST;
+	}
+
+	// create the plugin header
+
+	$header = <<<END
+/*
+Theme Name: {$_POST['rzct_name']}
+Template: {$_POST['rzct_template']}
+Description: {$_POST['rzct_description']}
+Version: {$_POST['rzct_version']}
+Author: {$_POST['rzct_author']}
+Author URI: {$_POST['rzct_author_uri']}
+*/
+
+@import url("../{$_POST['rzct_template']}/style.css");
+
+/* =Theme customization starts here
+--------------------------------- */
+END;
+
+	$themefile = trailingslashit($themedir).'style.css';
+
+	if ( ! $wp_filesystem->put_contents( $themefile, $header, FS_CHMOD_FILE) ) {
+		add_settings_error( 'rzct', 'create_file', esc_html__('Unable to create the theme file.', 'rzct'), 'error' );
+	}
+
+	$themeslug = $_POST['rzct_slug'].'/'.$_POST['rzct_slug'].'.css';
+	$themeeditor = admin_url('theme-editor.php?file=style.css&theme='.$_POST['rzct_slug']);
+
+	if ( null !== switch_theme( $_POST['rzct_template'], $_POST['rzct_slug']  ) ) {
+		add_settings_error( 'rzct', 'activate_theme', esc_html__('Unable to activate the new theme.', 'rzct'), 'error' );
+	}
+
+	// theme created and activated, redirect to the theme editor
+	?>
+	<script type="text/javascript">
+	<!--
+	window.location = "<?php echo $themeeditor; ?>"
+	//-->
+	</script>
+	<?php
+
+	/* translators: inline link to plugin editor */
+	$message = sprintf(esc_html__('The new theme has been created and activated. You can %sgo to the editor%s if your browser does not redirect you.', 'rzct'), '<a href="'.$themeeditor.'">', '</a>');
+
+	add_settings_error('rzct', 'theme_active', $message, 'rzct', 'updated');
+
+	return true;
+}
+
